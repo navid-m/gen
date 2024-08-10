8772daf125e9e36a20de8247027b712ddd65328b
Navid M <navidmomtahen@gmail.com>
2024-08-10 01:02:15 +0100

    rf

--- genjar.py
+++ genjar.py
@@ -1,25 +1,25 @@
 import os
 import sys
 
+script_name = "unknown"
+
 
 def generate_script(jar_path):
+    global script_name
     jar_abs_path = os.path.abspath(jar_path)
     jar_name = os.path.basename(jar_abs_path)
     shortened_name = jar_name.split('-')[0].replace(".jar", "")
     command = f"java --add-opens=java.base/java.lang=ALL-UNNAMED -jar {
         jar_abs_path} %* \n"
-
     if os.name == 'nt':
         script_name = f"{shortened_name}.cmd"
         with open(script_name, 'w') as script_file:
             script_file.write(f"@echo off\n{command}")
-    else:
-        script_name = f"{shortened_name}.sh"
-        with open(script_name, 'w') as script_file:
-            script_file.write(f"#!/bin/bash\n{command}")
-        os.chmod(script_name, 0o755)
-
-    print(f"Generated: {script_name}")
+        return
+    script_name = f"{shortened_name}.sh"
+    with open(script_name, 'w') as script_file:
+        script_file.write(f"#!/bin/bash\n{command}")
+    os.chmod(script_name, 0o755)
 
 
 if __name__ == "__main__":
@@ -27,4 +27,8 @@ if __name__ == "__main__":
         print("Usage: genjar <path_to_jar>")
     else:
         jar_path = sys.argv[1]
-        generate_script(jar_path)
+        if (os.path.exists(jar_path)):
+            generate_script(jar_path)
+            print(f"Generated: {script_name}")
+            raise SystemExit()
+        print("Specified JAR does not exist")
