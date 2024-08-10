15c7559b43009836b754c6711d1a3658d0efc122
Navid M <navidmomtahen@gmail.com>
2024-08-10 01:15:09 +0100

    remove glob

--- genjar.py
+++ genjar.py
@@ -1,40 +1,39 @@
 import os
 import sys
 
-out_name = ""
-
 
 def generate_script(jar_path):
-    global out_name
     jar_abs_path = os.path.abspath(jar_path)
-    display_name = os.path.basename(
-        jar_abs_path
-    ).split('-')[0].replace(".jar", "")
+    display_name = os.path.splitext(
+        os.path.basename(
+            jar_abs_path
+        )
+    )[0].split('-')[0]
 
-    command = f"java --add-opens=java.base/java.lang=ALL-UNNAMED -jar {
-        jar_abs_path} %* \n"
+    cmnd = f"java --add-opens=java.base/java.lang=ALL-UNNAMED -jar {
+        jar_abs_path} %*\n"
 
-    if os.name == 'nt':
-        out_name = f"{display_name}.cmd"
-        with open(out_name, 'w') as script_file:
-            script_file.write(f"@echo off\n{command}")
-        return
+    sc = f"@echo off\n{cmnd}" if os.name == 'nt' else f"#!/bin/bash\n{cmnd}"
+    out_name = f"{display_name}.{'cmd' if os.name == 'nt' else 'sh'}"
 
-    out_name = f"{display_name}.sh"
+    with open(out_name, 'w') as sc_file:
+        sc_file.write(sc)
 
-    with open(out_name, 'w') as script_file:
-        script_file.write(f"#!/bin/bash\n{command}")
+    if os.name != 'nt':
+        os.chmod(out_name, 0o755)
 
-    os.chmod(out_name, 0o755)
+    return out_name
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: genjar <path_to_jar>")
+        sys.exit(1)
+
+    jar_path = sys.argv[1]
+    if os.path.exists(jar_path):
+        out_name = generate_script(jar_path)
+        print(f"Generated: {out_name}")
     else:
-        jar_path = sys.argv[1]
-        if (os.path.exists(jar_path)):
-            generate_script(jar_path)
-            print(f"Generated: {out_name}")
-            raise SystemExit()
         print("Specified JAR does not exist")
+        sys.exit(1)
